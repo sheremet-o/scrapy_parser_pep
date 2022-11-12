28aaf085fde09642af8d6dc1b6966c89d9b18442
Oksana Sheremet <sheremet5631@gmail.com>
2022-11-12 13:13:45 +0300

    items and spider

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,5 +1,7 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
+
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
@@ -7,4 +9,15 @@ class PepSpider(scrapy.Spider):
     start_urls = ['http://peps.python.org/']
 
     def parse(self, response):
-        pass
+        for pep_link in response.css(
+                'section#numerical-index td a::attr(href)').getall():
+            yield response.follow(pep_link + '/', callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        pep = response.css('ul.breadcrumbs > li + li + li::text').get()
+        data = {
+            'number': pep.replace('PEP ', ''),
+            'name': response.css('#pep-content h1.page-title::text').get(),
+            'status': response.css('dt:contains("Status") + dd::text').get()
+        }
+        yield PepParseItem(data)
