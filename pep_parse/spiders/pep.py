261f6cc05574b4eae945b7cdc689315852f7416c
Oksana Sheremet <sheremet5631@gmail.com>
2022-11-13 14:07:04 +0300

    fixes

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -6,7 +6,7 @@ from pep_parse.items import PepParseItem
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['https://peps.python.org/']
+    start_urls = ['http://peps.python.org/']
 
     def parse(self, response):
         for pep_link in response.css(
